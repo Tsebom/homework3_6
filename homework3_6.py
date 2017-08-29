df989282b115e368546fe627427ae622c1ec4109
Nezhdanov Sergey <Tsebom@yandex.ru>
2017-08-29 23:23:12 +0400

    Классы и их применение в Python

--- homework3_6.py
+++ homework3_6.py
@@ -1,69 +1,151 @@
 class AnimalFarm:
-    def __init__(self, name, weight, age, type_animal, cry):
+    def __init__(self, name, weight, age):
         self.name = name
         self.weight = weight
         self.age = age
-        self.type_animal = type_animal
-        self.cry = cry
-
-    def how_cry(self):
-        print('{}'.format(self.cry))
 
 
 class Mammals(AnimalFarm):
-    def __init__(self, name, weight, age, type_animal, cry, homs):
-        super().__init__(name, weight, age, type_animal, cry)
+    def __init__(self, name, weight, age, homs):
+        super().__init__(name, weight, age)
         self.homs = homs
 
     def homs_is(self):
         if self.homs:
-            print('рогоносец')
-        elif self.age >= 3:
-            print('бекон')
+            return 'рогоносец'
         else:
-            print('поросенок')
+            return 'безрогое'
 
 
 class Birds(AnimalFarm):
-    def __init__(self, name, weight, age, type_animal,cry, fly, swim):
-        super().__init__(name, weight, age, type_animal, cry)
+    def __init__(self, name, weight, age, fly, swim):
+        super().__init__(name, weight, age)
         self.fly = fly
         self.swim = swim
 
     def fly_or_swim(self):
         if self.fly:
-            print('летающая')
+            return 'летающая'
         elif self.swim:
-            print('плавающая')
+            return 'плавающая'
         else:
-            print('бегающая')
+            return 'бегающая'
 
 
-cow = Mammals('Машка', 300, 5, 'корова', 'мычит', True)
+class Cow(Mammals):
 
-goat = Mammals('Сема', 20, 2, 'козел', 'блеет', True)
+    def __init__(self, name, weight, age, homs):
+        super().__init__(name, weight, age, homs)
 
-sheep = Mammals('Толик', 30, 3, 'баран', 'бекает', True)
+    def type_animal(self):
+        return 'корова'
 
-pig = Mammals('Борька', 50, 4, 'свин', 'хрюкает', False)
-
-mammals = [cow, goat, sheep, pig]
-for animal in mammals:
-    print('{} {} {}'.format(animal.type_animal, animal.name, animal.cry))
-    print('В {} живого веса на {} кг'.format(animal.name, animal.weight))
-    print('Отличительный признак ', end='- ')
-    print(animal.homs_is(), '\n')
+    def cry_animal(self):
+        return 'мычит'
 
 
-duck = Birds('Скрудж', 5, 1, 'утка', 'крякает', True, True)
+class Goat(Mammals):
 
-chicken = Birds('Ряба', 2, 1, 'курица', 'кудахчет', False, False)
+    def __init__(self, name, weight, age, homs):
+        super().__init__(name, weight, age, homs)
 
-goose = Birds('Степа', 10, 1, 'гусь', 'гогочет', True, True)
+    def type_animal(self):
+        return 'козел'
 
-birds = [duck, chicken, goose]
-for bird in birds:
-    print('{} {} {}'.format(bird.type_animal, bird.name, bird.cry))
-    print('В {} живого веса на {} кг'.format(bird.name, bird.weight))
-    print('Отличительный признак ', end='- ')
-    print(bird.fly_or_swim(), '\n')
+    def cry_animal(self):
+        return 'блеет'
+
+
+class Sheep(Mammals):
+
+    def __init__(self, name, weight, age, homs):
+        super().__init__(name, weight, age, homs)
+
+    def type_animal(self):
+        return 'баран'
+
+    def cry_animal(self):
+        return 'бекает'
+
+
+class Pig(Mammals):
+
+    def __init__(self, name, weight, age, homs):
+        super().__init__(name, weight, age, homs)
+
+    def type_animal(self):
+        return 'свин'
+
+    def cry_animal(self):
+        return 'хрюкает'
+
+
+class Duck(Birds):
+
+    def __init__(self, name, weight, age, fly, swim):
+        super().__init__(name, weight, age, fly, swim)
+
+    def type_animal(self):
+        return 'утка'
+
+    def cry_animal(self):
+        return 'крякает'
+
+
+class Chicken(Birds):
+
+    def __init__(self, name, weight, age, fly, swim):
+        super().__init__(name, weight, age, fly, swim)
+
+    def type_animal(self):
+        return 'курица'
+
+    def cry_animal(self):
+        return 'кудахчет'
+
+
+class Goose(Birds):
+
+    def __init__(self, name, weight, age, fly, swim):
+        super().__init__(name, weight, age, fly, swim)
+
+    def type_animal(self):
+        return 'гусь'
+
+    def cry_animal(self):
+        return 'гогочет'
+
+
+def main():
+    cow = Cow('Машка', 300, 5, True)
+
+    goat = Goat('Сема', 20, 2, True)
+
+    sheep = Sheep('Толик', 30, 3, True)
+
+    pig = Pig('Борька', 50, 4, False)
+
+    mammals = [cow, goat, sheep, pig]
+    for animal in mammals:
+        print('{} {} {}'.format(
+            animal.type_animal(), animal.name, animal.cry_animal()))
+        print('В {} живого веса на {} кг'.format(animal.name, animal.weight))
+        print('Отличительный признак ', end='- ')
+        print(animal.homs_is(), '\n')
+
+    duck = Duck('Скрудж', 5, 1, True, True)
+
+    chicken = Chicken('Ряба', 2, 1, False, False)
+
+    goose = Goose('Степа', 10, 1, True, True)
+
+    birds = [duck, chicken, goose]
+    for bird in birds:
+        print('{} {} {}'.format(
+            bird.type_animal(), bird.name, bird.cry_animal()))
+        print('В {} живого веса на {} кг'.format(bird.name, bird.weight))
+        print('Отличительный признак ', end='- ')
+        print(bird.fly_or_swim(), '\n')
+
+
+main()
